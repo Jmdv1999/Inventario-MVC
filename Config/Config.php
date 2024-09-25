228afef176fddbdc8ba49b17414b2005d085df9d
Jose Miguel Delgado Villasmil <91023157+Jmdv1999@users.noreply.github.com>
2024-09-25 08:29:14 -0400

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-    const base_url = "https://tudominio.com/"
+    const base_url = "https://tudominio.com/";
     const host = "DB_HOST";
     const usuario = "DB_USER";
     const pass = "DB_PAS";
